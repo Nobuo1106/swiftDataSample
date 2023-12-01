3cc70f37b2617806da3a9c1ef044d3b097569a03
五十嵐伸雄 <gonnbe1106@gmail.com>
2023-12-01 23:41:18 +0900

    Initial Commit

---
+++ swiftDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftDataSample
+//
+//  Created by 五十嵐伸雄 on 2023/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
